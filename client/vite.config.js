d099f1a62e64ef51fab3c99b2a30dd753edb9a67
CARA7XES <MuhammadZakarya862@gmail.com>
2026-05-23 22:25:13 +0500

    vite config modified

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://labease-swg3.onrender.com',
         changeOrigin: true,
       }
     }
